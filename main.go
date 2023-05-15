a4f1ed17256939c6f68cb160bbda2acd4e8e0936
Ashwin Anil <ashwinrockon123@gmail.com>
2023-05-15 13:52:29 +0530

    Added: Code for Connecting to db, testing the connection and closing the connection(defer)

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
 func main() {
 	// Connect to DB
+	conn, err := sql.Open("pgx", fmt.Sprintf("host=localhost port=5432 dbname=go-postgres user=%s password=%s", username, pass))
+
+	if err != nil {
+		log.Fatal("failed to connect: ", err)
+	}
 
 	// Test Connection
+	err = conn.Ping()
+
+	if err != nil {
+		log.Fatal("failed to ping database")
+	}
+
+	log.Println("Connected")
 
 	// Defer - close connection
+	defer conn.Close()
 
-	// Get all rows from table
+	// Get all rows from tasql
 
 	// Insert a row
 
