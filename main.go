4f0d537abb7069d1c1b95ff700d948e1e051e1b3
Ashwin Anil <ashwinrockon123@gmail.com>
2023-05-15 14:13:14 +0530

    Added: Query for getting all the rows from a table

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// Connect to DB
-	conn, err := sql.Open("pgx", fmt.Sprintf("host=localhost port=5432 dbname=go-postgres user=%s password=%s", username, pass))
+	conn, err := sql.Open("pgx", fmt.Sprintf("host=localhost port=5432 dbname=go-postgres user=%s password=%s", username, pass)) // Insert usernme and password
 
 	if err != nil {
 		log.Fatal("failed to connect: ", err)
@@ -28,7 +28,11 @@ func main() {
 	// Defer - close connection
 	defer conn.Close()
 
-	// Get all rows from tasql
+	// Get all rows from table
+	err = getRows(conn)
+	if err != nil {
+		log.Fatal("failed getRows()", err)
+	}
 
 	// Insert a row
 
@@ -43,4 +47,34 @@ func main() {
 	// Delete row by id
 
 	// Get all rows again
+}
+
+func getRows(conn *sql.DB) error {
+	// Executing the Select * query
+	rows, err := conn.Query("select * from users")
+
+	if err != nil {
+		log.Fatal("could not return rows", err)
+		return err
+	}
+
+	// Closing *sql.Rows
+	defer rows.Close()
+	var first_name, last_name string
+	var id int
+
+	log.Println("Getting all rows --------------------------------")
+	for rowId := 1; rows.Next(); rowId++ {
+		err := rows.Scan(&id, &first_name, &last_name)
+
+		if err != nil {
+			log.Fatal("could not read record")
+			return err
+		}
+
+		log.Printf("Row %d: %d %s %s\n", rowId, id, first_name, last_name)
+	}
+
+	log.Println("--------------------------------------------------")
+	return nil
 }
